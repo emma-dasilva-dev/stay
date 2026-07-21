f6dbba84efadffc2e16b6237372cdf41a0f1246f
emma <dasilvaemma219@gmail.com>
2026-07-21 23:33:05 +0100

    Fix Logo import casting for Vercel deployment

--- frontend/src/pages/EmployeePortal/EmployeePortal.jsx
+++ frontend/src/pages/EmployeePortal/EmployeePortal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import Logo from "../../components/Logo/logo";
+import Logo from "../../components/Logo/Logo";
 
 import {
   clearEmployeeSession,
